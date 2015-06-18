ccbc7899f115117b93f955e7c545c6292438c57a
Qiqi <zqmillet@qq.com>
2015-06-18 09:51:31 +0800

    Modify CodeTextBox

--- Qiqi.Reference/TextBoxTest/TextBox.vb
+++ Qiqi.Reference/TextBoxTest/TextBox.vb
@@ -17,15 +17,17 @@
             Reader.Close()
         End Sub
 
-        Private Sub Me_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Me.TextChanged
-            Me.Font = New Font(Me.Font.FontFamily, Me.Font.Size)
-            'MsgBox("")
-        End Sub
+        'Private Sub Me_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Me.TextInput
+        '    Me.Font = New Font(Me.Font.FontFamily, Me.Font.Size)
+        'End Sub
 
-        Private Sub Me_Invalidated(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Me.Invalidated
-            RemoveHandler Me.Invalidated, AddressOf Me_Invalidated
-            Me.Font = New Font(Me.Font.FontFamily, Me.Font.Size)
-        End Sub
+
+
+
+        'Private Sub Me_Invalidated(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Me.Invalidated
+        '    RemoveHandler Me.Invalidated, AddressOf Me_Invalidated
+        '    Me.Font = New Font(Me.Font.FontFamily, Me.Font.Size)
+        'End Sub
     End Class
 End Namespace
 
