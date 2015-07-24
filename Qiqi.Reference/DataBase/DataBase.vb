ab6931e70c0b5e51fe9eeea36ada2fab7c799f38
Qiqi <zqmillet@qq.com>
2015-07-24 22:06:50 +0800

    Modify Class DataBase

--- Qiqi.Reference/DataBase/DataBase.vb
+++ Qiqi.Reference/DataBase/DataBase.vb
@@ -1,14 +1,25 @@
 ﻿Namespace Qiqi
+    Public Enum DataBaseType
+        ' This is not a currect database type
+        ErrorType
+        ' the database has no extension
+        Unknown
+        ' the type of database is BibTeX 
+        BibTeX
+    End Enum
+
     Public Class DataBase
         Public LiteratureList As ArrayList
         Public Group As Qiqi.Group
         Public FullFileName As String
+        Public CompileResult As Qiqi.CompileResult
 
         Public Sub New()
             With Me
                 .LiteratureList = New ArrayList
                 .Group = New Qiqi.Group
                 .FullFileName = ""
+                .CompileResult = New Qiqi.CompileResult
             End With
         End Sub
 
@@ -17,11 +28,43 @@
                 .LiteratureList = New ArrayList
                 .Group = New Qiqi.Group
                 .FullFileName = FullFileName
+                .CompileResult = New Qiqi.CompileResult
             End With
         End Sub
 
         Public Sub Load()
-
+            GetDataBaseType()
         End Sub
+
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Private Function GetDataBaseType() As Qiqi.DataBaseType
+            If FullFileName.Trim = "" Then
+                Return Qiqi.DataBaseType.ErrorType
+            End If
+
+            If Not FullFileName.Contains("\") Then
+                Return Qiqi.DataBaseType.ErrorType
+            End If
+
+            Dim Extension As String = FullFileName.Remove(0, FullFileName.LastIndexOf("\") + 1)
+
+            If Not Extension.Contains(".") Then
+                Return Qiqi.DataBaseType.Unknown
+            End If
+
+            Extension = Extension.Remove(0, Extension.LastIndexOf(".") + 1)
+
+            Select Case Extension.Trim.ToLower
+                Case "bib"
+                    Return Qiqi.DataBaseType.BibTeX
+                Case Else
+                    Return Qiqi.DataBaseType.Unknown
+            End Select
+        End Function
     End Class
 End Namespace
--- Qiqi.Reference/Test/FormTest.vb
+++ Qiqi.Reference/Test/FormTest.vb
@@ -6,5 +6,8 @@
 
         'Me.Controls.Add(New _Test.TextBox)
         'Me.Size = New Size(800, 600)
+
+        Dim d As New Qiqi.DataBase("D:\iCloud\iPaper\Test.bib")
+        d.Load()
     End Sub
 End Class
