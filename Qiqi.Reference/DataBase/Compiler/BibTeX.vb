bc33ad043944c6dd3bcef299aec02fd9d7d8276e
Qiqi <zqmillet@qq.com>
2015-07-28 18:32:21 +0800

    Modify Class DataBase

--- Qiqi.Reference/DataBase/Compiler/BibTeX.vb
+++ Qiqi.Reference/DataBase/Compiler/BibTeX.vb
@@ -12,7 +12,7 @@
             Public Const FileNotExist As String = "File do not Exist!"
             Public Const IdleSyntaxError As String = "Idle Mode Syntax Error!"
             Public Const TypeSyntaxError As String = "Type Syntax Error!"
-            Public Const BibTeXSyntaxError As String = "BibTeX Syntax Error!"
+            Public Const BibTeXKeySyntaxError As String = "BibTeXKey Syntax Error!"
 
         End Module
 
@@ -36,6 +36,8 @@
                 Dim LiteratureID As String = ""
                 Dim LiteratureBuffer As String = ""
 
+                Dim BraketNumber As Integer = 0
+
                 ' Initialize the line number
                 DataBase.CompileResult.LineNumber = 1
 
@@ -51,11 +53,13 @@
                         Case BibTeXAnalysisState.Idle
                             Select Case c
                                 Case "%"
-                                    AnalysisState = BibTeXAnalysisState.Idle
+                                    AnalysisState = BibTeXAnalysisState.ReadComment
                                 Case "@"
                                     ' If c = "@", it means the begin of a literature,
                                     ' then initialize the information of a new literature.
-
+                                    LiteratureBuffer = ""
+                                    LiteratureID = ""
+                                    LiteratureType = ""
 
                                     AnalysisState = BibTeXAnalysisState.ReadType
                                 Case " "
@@ -70,13 +74,16 @@
                             End Select
                         Case BibTeXAnalysisState.ReadType
                             ' The type should not have the following char:
-                            ' % @ { } vbCr vbLf
+                            ' % @ { } vbCr vbLf ,
                             ' If c = "{", it means that the type has been read.
                             Select Case c
                                 Case "%"
                                     DataBase.SetErrorMessage(BibTeXErrorMessage.TypeSyntaxError)
                                     Exit Sub
                                 Case "@"
+                                    DataBase.SetErrorMessage(BibTeXErrorMessage.TypeSyntaxError)
+                                    Exit Sub
+                                Case ","
                                     DataBase.SetErrorMessage(BibTeXErrorMessage.TypeSyntaxError)
                                     Exit Sub
                                 Case "}"
@@ -89,13 +96,60 @@
                                     DataBase.SetErrorMessage(BibTeXErrorMessage.TypeSyntaxError)
                                     Exit Sub
                                 Case "{"
-
+                                    BraketNumber = 1
+                                    AnalysisState = BibTeXAnalysisState.ReadBibTeXKey
                                 Case Else
-
+                                    LiteratureType &= c
                             End Select
                         Case BibTeXAnalysisState.ReadBibTeXKey
+                            ' The BibTeXKey should not have the following char:
+                            ' % @ { } vbCr vbLf ,
+                            ' If c = ",", it means that the type has been read.
+                            Select Case c
+                                Case "%"
+                                    DataBase.SetErrorMessage(BibTeXErrorMessage.BibTeXKeySyntaxError)
+                                    Exit Sub
+                                Case "@"
+                                    DataBase.SetErrorMessage(BibTeXErrorMessage.BibTeXKeySyntaxError)
+                                    Exit Sub
+                                Case "{"
+                                    DataBase.SetErrorMessage(BibTeXErrorMessage.BibTeXKeySyntaxError)
+                                    Exit Sub
+                                Case "}"
+                                    DataBase.SetErrorMessage(BibTeXErrorMessage.BibTeXKeySyntaxError)
+                                    Exit Sub
+                                Case vbCr
+                                    DataBase.SetErrorMessage(BibTeXErrorMessage.BibTeXKeySyntaxError)
+                                    Exit Sub
+                                Case vbLf
+                                    DataBase.SetErrorMessage(BibTeXErrorMessage.BibTeXKeySyntaxError)
+                                    Exit Sub
+                                Case ","
+                                    AnalysisState = BibTeXAnalysisState.ReadBuffer
+                                Case Else
+                                    LiteratureID &= c
+                            End Select
                         Case BibTeXAnalysisState.ReadBuffer
+                            Select Case c
+                                Case "{"
+                                    BraketNumber += 1
+                                Case "}"
+                                    BraketNumber -= 1
+                                Case Else
+                                    LiteratureBuffer &= c
+                            End Select
+
+                            If BraketNumber = 0 Then
+                                LiteratureBuffer = LiteratureBuffer.Trim
+                                AnalysisState = BibTeXAnalysisState.Idle
+                            End If
                         Case BibTeXAnalysisState.ReadComment
+                            Select Case c
+                                Case vbCr
+                                    AnalysisState = BibTeXAnalysisState.Idle
+                                Case Else
+                                    ' Do nithing
+                            End Select
                     End Select
                 Next
                 Reader.Close()
