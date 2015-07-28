f6b86667461edcbd4dc8722f7902988be7f1f1e3
Qiqi <zqmillet@qq.com>
2015-07-28 18:00:19 +0800

    Modify Class DataBase

--- Qiqi.Reference/DataBase/Compiler/BibTeX.vb
+++ Qiqi.Reference/DataBase/Compiler/BibTeX.vb
@@ -10,27 +10,39 @@
 
         Public Module BibTeXErrorMessage
             Public Const FileNotExist As String = "File do not Exist!"
-            Public Const SyntaxError As String = "Syntax Error!"
+            Public Const IdleSyntaxError As String = "Idle Mode Syntax Error!"
+            Public Const TypeSyntaxError As String = "Type Syntax Error!"
+            Public Const BibTeXSyntaxError As String = "BibTeX Syntax Error!"
 
         End Module
 
         Public Class BibTeX
             Public Shared Sub Compile(ByRef DataBase As Qiqi.DataBase)
+                ' If there do not exist the database, exit sub
                 If Not My.Computer.FileSystem.FileExists(DataBase.FullFileName) Then
                     DataBase.SetErrorMessage(BibTeXErrorMessage.FileNotExist)
                     Exit Sub
                 End If
 
                 Dim Reader As New IO.StreamReader(DataBase.FullFileName, System.Text.Encoding.Default)
+
+                ' The buffer of database
                 Dim DataBaseBuffer As String = Reader.ReadToEnd
+                ' State of lexical analysis
                 Dim AnalysisState As Qiqi.Compiler.BibTeXAnalysisState = Qiqi.Compiler.BibTeXAnalysisState.Idle
 
+                ' The parameters of a literature
+                Dim LiteratureType As String = ""
+                Dim LiteratureID As String = ""
+                Dim LiteratureBuffer As String = ""
 
+                ' Initialize the line number
                 DataBase.CompileResult.LineNumber = 1
 
                 For Index As Integer = 0 To DataBaseBuffer.Length - 1
                     Dim c As Char = DataBaseBuffer(Index)
 
+                    ' Update the line number
                     If c = vbCr Then
                         DataBase.CompileResult.LineNumber += 1
                     End If
@@ -41,6 +53,10 @@
                                 Case "%"
                                     AnalysisState = BibTeXAnalysisState.Idle
                                 Case "@"
+                                    ' If c = "@", it means the begin of a literature,
+                                    ' then initialize the information of a new literature.
+
+
                                     AnalysisState = BibTeXAnalysisState.ReadType
                                 Case " "
                                     ' Do nothing
@@ -49,14 +65,32 @@
                                 Case vbLf
                                     ' Do nothing
                                 Case Else
-                                    DataBase.SetErrorMessage(BibTeXErrorMessage.SyntaxError)
+                                    DataBase.SetErrorMessage(BibTeXErrorMessage.IdleSyntaxError)
                                     Exit Sub
                             End Select
                         Case BibTeXAnalysisState.ReadType
+                            ' The type should not have the following char:
+                            ' % @ { } vbCr vbLf
+                            ' If c = "{", it means that the type has been read.
                             Select Case c
                                 Case "%"
+                                    DataBase.SetErrorMessage(BibTeXErrorMessage.TypeSyntaxError)
+                                    Exit Sub
                                 Case "@"
+                                    DataBase.SetErrorMessage(BibTeXErrorMessage.TypeSyntaxError)
+                                    Exit Sub
+                                Case "}"
+                                    DataBase.SetErrorMessage(BibTeXErrorMessage.TypeSyntaxError)
+                                    Exit Sub
+                                Case vbCr
+                                    DataBase.SetErrorMessage(BibTeXErrorMessage.TypeSyntaxError)
+                                    Exit Sub
+                                Case vbLf
+                                    DataBase.SetErrorMessage(BibTeXErrorMessage.TypeSyntaxError)
+                                    Exit Sub
+                                Case "{"
 
+                                Case Else
 
                             End Select
                         Case BibTeXAnalysisState.ReadBibTeXKey
