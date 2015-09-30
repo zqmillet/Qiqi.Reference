63927cd37c5e42565379026e1c3d68b6b86ad37b
zqmillet <zqmillet@qq.com>
2015-09-30 15:57:09 +0800

    Test

--- Qiqi.Reference/My Project/Application.Designer.vb
+++ Qiqi.Reference/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Qiqi.Reference.FormTest
+            Me.MainForm = Global.Qiqi.Reference.FormMain
         End Sub
     End Class
 End Namespace
